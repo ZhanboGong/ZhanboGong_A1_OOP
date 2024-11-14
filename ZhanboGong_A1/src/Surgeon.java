12d61db5df9c33c0efbcd5a5309f49a81ac8e338
ZhanboGong <z.gong.11@student.scu.edu.au>
2024-11-14 23:30:44 +0800

    Update Surgeon.java
    Update the printInformation method and the validation method

--- ZhanboGong_A1/src/Surgeon.java
+++ ZhanboGong_A1/src/Surgeon.java
@@ -1,4 +1,5 @@
 public class Surgeon extends HealthProfessional{
+    private final String professionalType = "Surgeon";
     private String surgicalField;
     public Surgeon()
     {
@@ -7,7 +8,7 @@ public class Surgeon extends HealthProfessional{
     public Surgeon(int id, String name, String officeAddress, String surgicalField)
     {
         super(id, name, officeAddress);
-        this.surgicalField = surgicalField;
+        setSurgicalField(surgicalField);
     }
     @Override
     public void printInformation()
@@ -15,6 +16,7 @@ public class Surgeon extends HealthProfessional{
         System.out.println("================================");
         System.out.println("Health Professional Information(Surgeon)");
         System.out.println("ID: " + getId());
+        System.out.println("Type: " + professionalType);
         System.out.println("Name: " + getName());
         System.out.println("Office Address: " + getOfficeAddress());
         System.out.println("Surgical Field: " + surgicalField);
@@ -29,11 +31,26 @@ public class Surgeon extends HealthProfessional{
 
     public void setSurgicalField(String surgicalField) {
         if(surgicalField == null || surgicalField.trim().isEmpty()){
-            System.out.println("Wrong Input: Surgical Field cannot be empty and null!");
+            if(this.surgicalField == null){
+                System.out.println("Wrong Input: Surgical Field cannot be empty and null! The current surgical field is set to 'To be set'");
+                this.surgicalField = "To be set";
+            }
+            else{
+                System.out.println("Wrong Input: Surgical Field cannot be empty and null! Surgical Field update failed, current surgical field is " + this.surgicalField);
+            }
         }
         else{
             this.surgicalField = surgicalField;
             System.out.println("The Surgical Field is successfully set to " + surgicalField);
         }
+    }
+
+    public String getProfessionalType(){
+        return professionalType;
+    }
+
+    @Override
+    public String toString(){
+        return "ID: " + getId() + "|" + "Name：" + getName();
     }
 }
