0c0b67c7650d9bdae9150921cb9a1f73ee3e0881
ZhanboGong <z.gong.11@student.scu.edu.au>
2024-11-14 23:48:29 +0800

    Update AssessmentOne.java
    Add the createAppointment method, printExistingAppointment, cancelBooking, and related validation fan methods.

--- ZhanboGong_A1/src/AssessmentOne.java
+++ ZhanboGong_A1/src/AssessmentOne.java
@@ -1,4 +1,9 @@
+import java.util.ArrayList;
+
 public class AssessmentOne {
+    private static ArrayList<HealthProfessional> healthProfessionals = new ArrayList<>();
+    private static ArrayList<Appointment> appointments = new ArrayList<>();
+    private static int appointmentIDCount = 1;
     public static void main(String[] args) {
         // Part 3 - Using classes and objects
         GeneralPractitioner generalPractitioner1 = new GeneralPractitioner(1, "John", "Building-No.6-North-301", true);
@@ -12,5 +17,58 @@ public class AssessmentOne {
         surgeon1.printInformation();
         surgeon2.printInformation();
         System.out.println("------------------------------");
+    }
+    public static void createAppointment(String name, String mobilePhone, String timeSlot, HealthProfessional healthProfessional){
+        //判断是否符合标准
+        Appointment appointment = new Appointment(appointmentIDCount++, name, mobilePhone, timeSlot,healthProfessional);
+        appointments.add(appointment);
+    }
+
+    public static void printExistingAppointments(){
+        if(appointments.isEmpty()){
+            System.out.println("The appointment list is empty!");
+        }
+        else{
+            for (Appointment appointment : appointments) {
+                appointment.printInformation();
+            }
+        }
+    }
+
+    public static void cancelBooking(String mobilePhone){
+        for(int i = appointments.size() - 1; i >= 0; i--){
+            if(appointments.get(i).getMobilePhone().equals(mobilePhone)){
+                Appointment appointment = appointments.get(i);
+                appointments.remove(i);
+                System.out.println("The appointment with ID " + appointment.getAppointmentID() + " and phone number " + mobilePhone + " has been deleted");
+            }
+            else{
+                System.out.println("Sorry, we can't find the reservation with the corresponding information. Please confirm your information again.");
+            }
+        }
+    }
+
+    public static ArrayList<HealthProfessional> getHealthProfessionals() {
+        return healthProfessionals;
+    }
+
+    public static void setHealthProfessionals(ArrayList<HealthProfessional> healthProfessionals) {
+        AssessmentOne.healthProfessionals = healthProfessionals;
+    }
+
+    public static ArrayList<Appointment> getAppointments() {
+        return appointments;
+    }
+
+    public static void setAppointments(ArrayList<Appointment> appointments) {
+        AssessmentOne.appointments = appointments;
+    }
+
+    public static int getAppointmentIDCount() {
+        return appointmentIDCount;
+    }
+
+    public static void setAppointmentIDCount(int appointmentIDCount) {
+        AssessmentOne.appointmentIDCount = appointmentIDCount;
     }
 }
