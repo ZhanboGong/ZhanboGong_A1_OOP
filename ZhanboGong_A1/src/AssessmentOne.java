1d075c1117937ccd40e0e6d5d78c5abd7d940025
ZhanboGong <z.gong.11@student.scu.edu.au>
2024-11-19 22:55:53 +0800

    Update AssessmentOne.java
    CreateAppointment updated methods, rewrite wrote a needed parameter printExistingAppointments method is used to print a single booking information, delete the set and get methods, do not need to set the storage reservation list for the final

--- ZhanboGong_A1/src/AssessmentOne.java
+++ ZhanboGong_A1/src/AssessmentOne.java
@@ -1,9 +1,17 @@
 import java.util.ArrayList;
 
+/**
+ * This class demonstrates the management of appointments
+ * Including creation, printing and deletion
+ * Test the code in the main method
+ *
+ */
 public class AssessmentOne {
-    private static ArrayList<HealthProfessional> healthProfessionals = new ArrayList<>();
-    private static ArrayList<Appointment> appointments = new ArrayList<>();
+    //
+    private static final ArrayList<Appointment> appointments = new ArrayList<>();
+    // Set the initial ID of Appointment to 1 and increment it each time a new Appointment is created
     private static int appointmentIDCount = 1;
+
     public static void main(String[] args) {
         // Part 3 - Using classes and objects
         GeneralPractitioner generalPractitioner1 = new GeneralPractitioner(1, "John", "Building-No.6-North-301", true);
@@ -17,13 +25,43 @@ public class AssessmentOne {
         surgeon1.printInformation();
         surgeon2.printInformation();
         System.out.println("------------------------------");
+        // Part 5
+        createAppointment("Zhanbo Gong", "1234567890", "11:00am-12:00am", generalPractitioner1);
+        createAppointment("Bob", "2134658700", "3:00pm-4:00pm", generalPractitioner2);
+        createAppointment("Jacob", "1387703323", "9:00am-10:00am", surgeon1);
+        createAppointment("Li Tian", "1517777323", "5:00pm-6:00pm", surgeon2);
+        printExistingAppointments();
+        cancelBooking("1387703323");
+        printExistingAppointments();
+        System.out.println("------------------------------");
+
+        // Other testing scripts
+        /*
+        surgeon1.recordSurgery("Jacob", "2024-12-12", "Building-No.6-South-404");
+        surgeon1.recordSurgery("Alice", "2024-12-13", "Building-No.6-South-404");
+        surgeon1.getSurgeryInformation("1");
+        surgeon1.cancelSurgery("1");
+        surgeon1.getSurgeryInformation("1");
+         */
+
     }
+
+    /**
+     * Created appointment, and validation of the data entered is included in the Appointment class
+     *
+     * @param name: Represents the patient name in the Appointment
+     * @param mobilePhone: Represents the patient mobile phone in the Appointment
+     * @param timeSlot: Represents the time slot of the Appointment
+     * @param healthProfessional: Represents the health professional of the Appointment
+     */
     public static void createAppointment(String name, String mobilePhone, String timeSlot, HealthProfessional healthProfessional){
-        //判断是否符合标准
         Appointment appointment = new Appointment(appointmentIDCount++, name, mobilePhone, timeSlot,healthProfessional);
         appointments.add(appointment);
     }
 
+    /**
+     * Print all appointments' information
+     */
     public static void printExistingAppointments(){
         if(appointments.isEmpty()){
             System.out.println("The appointment list is empty!");
@@ -35,33 +73,38 @@ public class AssessmentOne {
         }
     }
 
+    public static void printExistingAppointments(int appointmentID){
+        boolean findingStatus = false;
+        for(int i = appointments.size() - 1; i >= 0; i--){
+            if(appointments.get(i).getAppointmentID() == appointmentID){
+                appointments.get(i).printInformation();
+                findingStatus = true;
+                break;
+            }
+        }
+        if(!findingStatus){
+            System.out.println("Sorry, we can't find the reservation with the corresponding information. Please confirm your information again.");
+        }
+    }
+
+    /**
+     * Find all reservations with the same phone number and delete them.
+     *
+     * @param mobilePhone: Represents the patient mobile phone in the Appointment
+     */
     public static void cancelBooking(String mobilePhone){
+        boolean findingStatus = false;
         for(int i = appointments.size() - 1; i >= 0; i--){
             if(appointments.get(i).getMobilePhone().equals(mobilePhone)){
                 Appointment appointment = appointments.get(i);
                 appointments.remove(i);
+                findingStatus = true;
                 System.out.println("The appointment with ID " + appointment.getAppointmentID() + " and phone number " + mobilePhone + " has been deleted");
             }
-            else{
-                System.out.println("Sorry, we can't find the reservation with the corresponding information. Please confirm your information again.");
-            }
         }
-    }
-
-    public static ArrayList<HealthProfessional> getHealthProfessionals() {
-        return healthProfessionals;
-    }
-
-    public static void setHealthProfessionals(ArrayList<HealthProfessional> healthProfessionals) {
-        AssessmentOne.healthProfessionals = healthProfessionals;
-    }
-
-    public static ArrayList<Appointment> getAppointments() {
-        return appointments;
-    }
-
-    public static void setAppointments(ArrayList<Appointment> appointments) {
-        AssessmentOne.appointments = appointments;
+        if(!findingStatus){
+            System.out.println("Sorry, we can't find the reservation with the corresponding information. Please confirm your information again.");
+        }
     }
 
     public static int getAppointmentIDCount() {
