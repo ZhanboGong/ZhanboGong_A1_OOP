d6e2f849b12b2c454df2759a8826a8de4aec9557
ZhanboGong <z.gong.11@student.scu.edu.au>
2024-11-19 22:34:02 +0800

    Update Surgeon.java
    Updated comments for classes, methods, etc

--- ZhanboGong_A1/src/Surgeon.java
+++ ZhanboGong_A1/src/Surgeon.java
@@ -1,20 +1,45 @@
 import java.util.ArrayList;
 
+/**
+ * The Surgeon class inherits from the abstract class HealthProfessional, the SurgicalArrangement interface is implemented
+ * The attribute visiting service status belonging to Surgeon is extended
+ * It provides empty and parameter constructors, as well as getters and setters to validate and read data.
+ * Override the printInformation method and the methods in interfaces
+ */
 public class Surgeon extends HealthProfessional implements SurgicalArrangement{
+    // It is used to store surgical records
     private final ArrayList<String> surgerys = new ArrayList<>();
+    // Unique identifiers for surgical records to facilitate automatic incrementing
     private int surgeryID = 1;
+    // Describe the type of the doctor as "Surgeon"
     private final String professionalType = "Surgeon";
+    // Represents the surgical field of the Surgeon(Private properties)
     private String surgicalField;
+    // No-argument constructors
     public Surgeon()
     {
         super();
     }
+
+    /**
+     *
+     * @param id: Represents the ID of the Surgeon
+     * @param name: Represents the name of the Surgeon
+     * @param officeAddress: Represents the office address of the Surgeon
+     * @param surgicalField: Represents the surgical field of the Surgeon
+     */
     public Surgeon(int id, String name, String officeAddress, String surgicalField)
     {
         super(id, name, officeAddress);
         setSurgicalField(surgicalField);
     }
 
+    /**
+     * Override the recordSurgery method in the Surgical Arrangement interface
+     * @param patient： patient name
+     * @param date: date of surgery
+     * @param address: address of surgery
+     */
     @Override
     public void recordSurgery(String patient, String date, String address){
         String surgeryRecord = surgeryID++ + ":" + patient + " underwent surgery in the " + address + " on " + date + ".";
@@ -22,6 +47,10 @@ public class Surgeon extends HealthProfessional implements SurgicalArrangement{
         System.out.println("The surgery record was added successfully!");
     }
 
+    /**
+     * Override the cancelSurgery method in the Surgical Arrangement interface
+     * @param surgeryID： A unique identifier that identifies the surgery
+     */
     @Override
     public void cancelSurgery(String surgeryID){
         boolean findingStatus = false;
@@ -38,6 +67,10 @@ public class Surgeon extends HealthProfessional implements SurgicalArrangement{
         }
     }
 
+    /**
+     * Override the getSurgeryInformation method in the Surgical Arrangement interface
+     * @param surgeryID： A unique identifier that identifies the surgery
+     */
     @Override
     public void getSurgeryInformation(String surgeryID){
         boolean findingStatus = false;
@@ -53,6 +86,9 @@ public class Surgeon extends HealthProfessional implements SurgicalArrangement{
         }
     }
 
+    /**
+     * Override the abstract method printInformation to print information about the Surgeon object
+     */
     @Override
     public void printInformation()
     {
@@ -68,10 +104,15 @@ public class Surgeon extends HealthProfessional implements SurgicalArrangement{
         System.out.println();
     }
 
+    // Used to get a Surgeon surgical field
     public String getSurgicalField() {
         return surgicalField;
     }
 
+    /**
+     * Used to set the new surgical field and verify that the surgical field is not null and empty
+     * @param surgicalField: Represents the surgical field of the Surgeon
+     */
     public void setSurgicalField(String surgicalField) {
         if(surgicalField == null || surgicalField.trim().isEmpty()){
             if(this.surgicalField == null){
@@ -88,10 +129,15 @@ public class Surgeon extends HealthProfessional implements SurgicalArrangement{
         }
     }
 
+    // Used to get the professional type(Surgeon)
     public String getProfessionalType(){
         return professionalType;
     }
 
+    /**
+     * Override the toString method
+     * @return surgeon id and surgeon name
+     */
     @Override
     public String toString(){
         return "ID: " + getId() + "|" + "Name：" + getName();
